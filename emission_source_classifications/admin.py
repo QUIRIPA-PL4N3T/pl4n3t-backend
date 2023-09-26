a651ac95aad5f9ad413be3e20e6867244332c4a7
Edwin Moreno <edwinmoreno328@gmail.com>
2023-09-26 07:37:34 -0500

    refactor/remove load initial data

--- emission_source_classifications/admin.py
+++ emission_source_classifications/admin.py
@@ -20,3 +20,12 @@ class GHGScopeAdmin(admin.ModelAdmin):
 @admin.register(EmissionSourceGroup)
 class EmissionSourceGroupAdmin(admin.ModelAdmin):
     filter_horizontal = ('emission_factor_types',)
+    list_display = ('id', 'name', 'description', 'allow_inventory', 'icon_tag')
+    list_editable = ('name', 'description', 'allow_inventory')
+
+    def icon_tag(self, obj):
+        if obj.icon:
+            return format_html('<img src="{}" style="max-width: 30px; max-height: 30px;" />'.format(obj.icon.url))
+        return ""
+
+    icon_tag.short_description = _('Ícono')
