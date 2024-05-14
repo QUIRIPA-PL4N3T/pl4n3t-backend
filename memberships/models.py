94ac280708a6c87c97f523f50bc16865449735cd
Edwin Moreno <edwinmoreno328@gmail.com>
2024-05-14 17:25:14 -0500

    feature/increase membership attributes

--- memberships/models.py
+++ memberships/models.py
@@ -7,15 +7,16 @@ class Membership(models.Model):
     FREE = 'FREE'
     BASIC = 'BASIC'
     PREMIUM = 'PREMIUM'
-    VIP = 'VIP'
+    ELITE = 'ELITE'
 
     MEMBERSHIP_CHOICES = [
-        (FREE, _('Free')),
+        (FREE, _('Gratuito')),
         (BASIC, _('Básica')),
         (PREMIUM, _('Premium')),
-        (VIP, _('VIP')),
+        (ELITE, _('Elite')),
     ]
     name = models.CharField(_('Nombre'), max_length=255)
+
     membership_type = models.CharField(
         _('Tipo de Membresía'),
         max_length=50,
@@ -23,9 +24,52 @@ class Membership(models.Model):
         default=FREE
     )
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    duration = models.IntegerField(help_text=_('Duración en días'), default=365)
+    duration = models.IntegerField(_('Duración en días'), default=365)
     description = models.TextField(blank=True, null=True)
     benefits = models.TextField(blank=True, null=True)
+    num_brands = models.IntegerField(_('Número de Marcas'), default=1)
+    num_locations = models.IntegerField(_('Número de Sedes'), default=1)
+    num_users = models.IntegerField(_('Usuarios por organización'), default=1)
+    emission_sources = models.BooleanField(_('Registro de todas las Fuentes de emisión'), default=True)
+    footprint_types = models.CharField(_('Tipos de huella'), max_length=255, default='Huella de Carbono')
+    liquidation_options = models.CharField(
+        _('Liquidación: Mensual, Trimestral, Anual'),
+        max_length=255,
+        default='Anual'
+    )
+    analysis_tools = models.BooleanField(
+        _('Herramientas de análisis y generación de informes personalizados'),
+        default=False
+    )
+    basic_support = models.BooleanField(
+        _('Acceso al soporte básico a través de preguntas frecuentes o correo electrónico'),
+        default=True
+    )
+    storage_capacity = models.CharField(
+        _('Mayor capacidad de almacenamiento para archivos y datos'),
+        max_length=255,
+        default='No sube soportes'
+    )
+    tutorials = models.BooleanField(
+        _('Tutoriales'),
+        default=True
+    )
+    webinars = models.BooleanField(
+        _('Webinars, documentación especializada, etc.'),
+        default=False
+    )
+    general_support = models.BooleanField(
+        _('Soporte técnico General'),
+        default=True
+    )
+    dedicated_support = models.BooleanField(
+        _('Un representante de soporte asignado exclusivamente a la cuenta'),
+        default=False
+    )
+    custom_api_access = models.BooleanField(
+        _('Acceso a una API personalizada para desarrollar integraciones personalizadas'),
+        default=False
+    )
 
     def __str__(self):
         return self.name
--- memberships/serializers.py
+++ memberships/serializers.py
@@ -5,7 +5,12 @@ from memberships.models import Membership, CompanyMembership
 class MembershipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Membership
-        fields = ['id', 'name', 'membership_type', 'price', 'duration', 'description', 'benefits']
+        fields = [
+            'id', 'name', 'membership_type', 'price', 'duration', 'description', 'benefits',
+            'num_brands', 'num_locations', 'num_users', 'emission_sources', 'footprint_types',
+            'liquidation_options', 'analysis_tools', 'basic_support', 'storage_capacity',
+            'tutorials', 'webinars', 'general_support', 'dedicated_support', 'custom_api_access'
+        ]
 
 
 class CompanyMembershipSerializer(serializers.ModelSerializer):
