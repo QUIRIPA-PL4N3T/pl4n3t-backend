349a5f79ff4344c7b85ffbe126a06a36cf9b9a8e
Edwin Moreno <edwinmoreno328@gmail.com>
2024-06-28 11:16:46 -0500

    Feature/add documents in emission source api

--- activities/api.py
+++ activities/api.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets, status
 from rest_framework.pagination import PageNumberPagination
@@ -38,6 +40,15 @@ class CustomPagination(PageNumberPagination):
     page_size = 25
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('total_pages', self.page.paginator.num_pages),
+            ('results', data)
+        ]))
 
 
 class ActivityFilter(filters.FilterSet):
